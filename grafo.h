e204787b69119394f8f6d303a87c023139c935be
StelvioGabriel <114026483+StelvioGabriel@users.noreply.github.com>
2026-07-08 21:36:52 +0100

    Refactor struct definitions for clarity

--- grafo.h
+++ grafo.h
@@ -1,30 +1,32 @@
+
 #define MAX_ESTACOES 50
 
-typedef struct Cano
-{
+
+typedef struct Cano {
     int id_destino;
     int capacidade;
     struct Cano* proximo;
 } Cano;
 
-typedef struct Estacao
-{
+typedef struct Estacao {
     char nome[100];
     char tipo[50];
     Cano* primeiro_cano;
 } Estacao;
 
-typedef struct RedeDeAgua
-{
+typedef struct RedeDeAgua {
     int num_estacoes;
     Estacao* estacoes[MAX_ESTACOES];
 } RedeDeAgua;
 
+
 RedeDeAgua* inicializar_rede();
 int adicionar_estacao(RedeDeAgua* rede, const char* nome, const char* tipo);
 void adicionar_cano(RedeDeAgua* rede, int id_origem, int id_destino, int capacidade);
+void listar_estacoes(const RedeDeAgua* rede);
+int guardar_dados_txt(RedeDeAgua* rede, const char* arquivo);
+RedeDeAgua* carregar_dados_txt(const char* arquivo);
 void encontrar_rota_agua(RedeDeAgua* rede, int id_origem, int id_destino);
-int guardar_dados_txt(RedeDeAgua* rede, const char* nome_arquivo);
-RedeDeAgua* carregar_dados_txt(const char* nome_arquivo);
-void ler_dados_txt(const char* nome_arquivo);
 void liberar_rede(RedeDeAgua* rede);
+
+
